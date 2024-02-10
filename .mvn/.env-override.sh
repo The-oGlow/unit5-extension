4698a7e305a7b5b3f45f4ae34918933c7d77b191
ollily <coding@glowa-net.com>
2024-02-10 20:36:01 +0100

    [all] : Configure gitlab for distribution

--- .mvn/.env-override.sh
+++ .mvn/.env-override.sh
@@ -3,4 +3,5 @@
 echo "Prior configure override settings"
 
 GITHUB_PROJECT_DIR=$(realpath "${SCRIPT_FOLDER}/..")
+export GITHUB_BRANCH_NAME="develop"
 GITHUB_REPO_NAME=$(basename "${GITHUB_PROJECT_DIR}")
